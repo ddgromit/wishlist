272086d6ab3461558bbcd3d7556082519f4fa6be
Derek Dahmer <derekdahmer@gmail.com>
2013-06-26 13:59:13 -0400

    Yi-wen

--- html/app.js
+++ html/app.js
@@ -1,3 +1,16 @@
 $(function() {
-	console.log('hey');
+	$.getJSON("https://generalassemb.ly/api/courses.js?jsonp=?",
+		function(data, textStatus, jqXHR){
+			var results = {};
+			$.each(data, function(index, value) {
+				$("#courses ul").append('<li>' + value.title + "</li>");
+				results[value.title] = value.id;
+			});
+
+			$("#courses ul").on("click", "li", function(event){
+				var liId = results[$(this).html()];
+				$("#wishlist ul").append("<li>" + $(this).html() + " - " + liId + "</li>");
+			});
+		});
+	
 });
